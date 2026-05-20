6c298438b209ab1b6a562aea0f108d2ea09f3862
Amos Steven Chacha <benzino559@gmail.com>
2026-05-20 22:49:28 +0000

    Update QR code to new DJ profile

--- app/components/QRCodeBox.tsx
+++ app/components/QRCodeBox.tsx
@@ -3,9 +3,8 @@
 import QRCode from "react-qr-code";
 
 export default function QRCodeBox() {
-  const requestUrl =
-    "https://dj-request-app-topaz.vercel.app/benzino";
-
+    const requestUrl =
+    "https://dj-request-app-topaz.vercel.app/djbenzino99";
   return (
     <div className="bg-zinc-900 p-6 rounded-2xl border border-zinc-800 text-center">
       <h2 className="text-2xl font-bold mb-4">
